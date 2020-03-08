14a3bb10e4a0bbc97e4fa085fcf0a39392e242ad
Erasto Vargas <thesmash00@gmail.com>
2020-03-07 21:27:42 -0600

    Updates

---
+++ precache-manifest.77aca419dc65022ef1edfd7610f17f9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31a15854f99ea7f09e7d5e9f2261383e",
+    "url": "/BertoniSolutionsAssessment/index.html"
+  },
+  {
+    "revision": "03916e55c8ddb6a4ff0c",
+    "url": "/BertoniSolutionsAssessment/static/css/main.89bd85ae.chunk.css"
+  },
+  {
+    "revision": "19abf6aa480423dede81",
+    "url": "/BertoniSolutionsAssessment/static/js/2.3e6ba291.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/BertoniSolutionsAssessment/static/js/2.3e6ba291.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03916e55c8ddb6a4ff0c",
+    "url": "/BertoniSolutionsAssessment/static/js/main.da8fb5db.chunk.js"
+  },
+  {
+    "revision": "c30042e3ae6386c4d6fb",
+    "url": "/BertoniSolutionsAssessment/static/js/runtime-main.269c90d2.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/BertoniSolutionsAssessment/static/media/logo.25bf045c.svg"
+  }
+]);
